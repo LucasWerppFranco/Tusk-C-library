55baaaf6e41450de187687e85e2f3534e0bf95c5
Lucas Franco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-04-27 17:19:12 -0300

    Update tusk.h

--- tusk.h
+++ tusk.h
@@ -1,13 +1,10 @@
 #ifndef TUSK_H
 #define TUSK_H
 
-// Função para configurar o terminal em modo não bloqueante
 void set_conio_terminal_mode();
 
-// Função para restaurar as configurações do terminal
 void reset_terminal_mode();
 
-// Função para verificar se há um caractere disponível para leitura
 int kbhit();
 
-#endif // CONIO_LIB_H
+#endif 

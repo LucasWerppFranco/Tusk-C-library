3d92b356997450ae33d181e3f1742cdd39a3d443
Lucas Franco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-04-27 17:18:49 -0300

    Update tusk.c

--- tusk.c
+++ tusk.c
@@ -7,41 +7,38 @@
 #include <fcntl.h>
 #include "tusk.h"
 
-// Função para configurar o terminal em modo não bloqueante
 void set_conio_terminal_mode() {
     struct termios new_settings;
-    tcgetattr(STDIN_FILENO, &new_settings); // Obtém as configurações atuais do terminal
-    new_settings.c_lflag &= ~(ICANON | ECHO); // Desativa o modo canônico e o eco
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings); // Aplica as novas configurações
+    tcgetattr(STDIN_FILENO, &new_settings); 
+    new_settings.c_lflag &= ~(ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings); 
 }
 
-// Função para restaurar as configurações do terminal
 void reset_terminal_mode() {
     struct termios new_settings;
-    tcgetattr(STDIN_FILENO, &new_settings); // Obtém as configurações atuais do terminal
-    new_settings.c_lflag |= (ICANON | ECHO); // Restaura o modo canônico e o eco
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings); // Aplica as novas configurações
+    tcgetattr(STDIN_FILENO, &new_settings); 
+    new_settings.c_lflag |= (ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_settings); 
 }
 
-// Função para verificar se há um caractere disponível para leitura
 int kbhit() {
     struct termios oldt, newt;
     int oldf;
-    tcgetattr(STDIN_FILENO, &oldt); // Obtém as configurações atuais do terminal
-    newt = oldt; // Copia as configurações
-    newt.c_lflag &= ~(ICANON | ECHO); // Desativa o modo canônico e o eco
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Aplica as novas configurações
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0); // Obtém as flags atuais
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK); // Define o modo não bloqueante
+    tcgetattr(STDIN_FILENO, &oldt); 
+    newt = oldt; 
+    newt.c_lflag &= ~(ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0); 
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK); 
 
-    int ch = getchar(); // Tenta ler um caractere
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Restaura as configurações do terminal
-    fcntl(STDIN_FILENO, F_SETFL, oldf); // Restaura as flags
+    int ch = getchar(); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+    fcntl(STDIN_FILENO, F_SETFL, oldf); 
 
-    if(ch != EOF) { // Se um caractere foi lido
-        ungetc(ch, stdin); // Coloca o caractere de volta no buffer
-        return 1; // Retorna 1 para indicar que há um caractere disponível
+    if(ch != EOF) { 
+        ungetc(ch, stdin); 
+        return 1; 
     }
 
-    return 0; // Retorna 0 se não houver caractere disponível
+    return 0; 
 }
